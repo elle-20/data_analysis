732d825ccb5879330fca471d600ed32ac205763d
elle-20 <eleonora.marina@edu.unito.it>
2022-04-16 18:02:01 +0200

    Added also the final output of the homework (cpm file)

--- L5.R
+++ L5.R
@@ -1,6 +1,7 @@
-#set correct working directory
-setwd("C:/Users/eleon/Desktop/data_analysis")
-#put the tab delimited file in an R object to be used with R - a variable
+# set correct working directory, neeed when working in RStudio, but we are doing it on the docker
+#setwd("\home\data_analysis")
+
+# put the tab delimited file in an R object to be used with R - a variable
 x <- read.table("L5dataset.txt", sep = "\t", header = TRUE, row.names = 1)
 
 #x is now a dataframe containing the data from the tab delimited file
@@ -8,7 +9,7 @@ x <- read.table("L5dataset.txt", sep = "\t", header = TRUE, row.names = 1)
 y <- data.matrix(x, rownames.force = NA)
 
 #retrieve the cpm of the new matrix
-cpm (y)
+z <- edgeR::cpm(y)
 
 #save the changes in a file
-write.table(y, file = "L5cpm.txt", append = FALSE, quote = TRUE, sep = "\t", col.names = NA)
+write.table(z, file = "L5cpm.txt", append = FALSE, quote = TRUE, sep = "\t", col.names = NA)
